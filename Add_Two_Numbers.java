903fbf59ed7814641f6371437bda3b2c7885d2d7
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2024-12-04 23:41:43 +0530

    Update Add_Two_Numbers.java

--- Add_Two_Numbers.java
+++ Add_Two_Numbers.java
@@ -1,4 +1,5 @@
 /**
+
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -7,6 +8,7 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ 
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
